34290a4b7da16f6db76c9e067ed20f14d76e0c20
GiacomoTezza <tezza.giacomo01@gmail.com>
2018-06-26 16:44:43 +0200

    Clien Push
    * Added read_hr_request (holding register readed every 10 minutes)
    * Added key pressed event check for break the loop
    * PEP8 and comments :)

--- test_client.py
+++ test_client.py
@@ -1,34 +1,73 @@
-from time import sleep
+# Import
+
 from pymodbus.client.sync import ModbusTcpClient
 from pymodbus.register_read_message import ReadRegistersRequestBase, ReadHoldingRegistersRequest, ReadHoldingRegistersResponse
 from pymodbus.factory import ClientDecoder
+
+from time import localtime, asctime
 import logging
+import keyboard
+
+# Log
+
 FORMAT = ('%(asctime)-15s %(threadName)-15s '
           '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-def start_client(address, port):
+# Defining server function
+
+def start_client(address, port=502):
 	"""Function that start a client
 
 	address: the address to witch the client is going to connect
 	port: the port of the server to witch the client is going to connect
 	"""
-	decoder = ClientDecoder()	
+	decoder = ClientDecoder()
 
 	# Starting and connecting the client to the server
 	
 	client = ModbusTcpClient(address, port)
 	client.connect()
-	
-	#request = ReadHoldingRegistersRequest(32, 1)
-	#response = client.execute(request)
-	#print(response.registers)
-	sleep(10)
+
+	read_hr_request(0, 5, client)
 
 	client.close()
 
+def read_hr_request(address, count, client):
+	"""Function that read a holding register every 10 minutes
+	
+	address: the starting address of the registers
+	count: the number of register to read after the first one
+	client: the istance of the client to use
+	"""
+	first = True
+	close = False
+	print('Press "q" to Quit: ')
+
+	# Loop start
+
+	while not close:
+		now = asctime(localtime()) # Current local time 
+
+		if int(now.split(" ")[3].split(":")[1]) % 2 == 0 and first:
+			request = ReadHoldingRegistersRequest(address, count)
+			response = client.execute(request)
+			print("[" + now + "] " + str(response.registers))
+			first = not first
+		
+		elif int(now.split(" ")[3].split(":")[1]) % 10 == 0 and not first:
+			first = True
+
+		# Key pressed event check
+
+		try:
+			if keyboard.is_pressed('q'):
+				close = not close
+		except:
+			pass
+
 	
 if __name__ == "__main__":
-	start_client('192.168.9.101', 502)
+	start_client('192.168.9.113', 502)
