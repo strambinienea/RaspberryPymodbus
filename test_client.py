3bfa9738c395ec777467acb89ed3eebf18dfff60
GiacomoTezza <tezza.giacomo01@gmail.com>
2018-06-27 11:38:17 +0200

    Client Improve
    Added:
    * Register suddivision

--- test_client.py
+++ test_client.py
@@ -3,6 +3,7 @@
 from pymodbus.client.sync import ModbusTcpClient
 from pymodbus.register_read_message import ReadRegistersRequestBase, ReadHoldingRegistersRequest, ReadHoldingRegistersResponse
 from pymodbus.factory import ClientDecoder
+from pymodbus.bit_read_message import ReadCoilsRequest, ReadCoilsResponse
 
 from time import localtime, asctime
 import logging
@@ -31,33 +32,48 @@ def start_client(address, port=502):
 	client = ModbusTcpClient(address, port)
 	client.connect()
 
-	read_hr_request(0, 5, client)
+	read_hr_request(register_index(1, 'T', 2), client)
 
 	client.close()
 
-def read_hr_request(address, count, client):
+def read_hr_request(address, client, count=0):
 	"""Function that read a holding register every 10 minutes
-	
+
 	address: the starting address of the registers
 	count: the number of register to read after the first one
 	client: the istance of the client to use
 	"""
 	first = True
 	close = False
+	delay = 2
 	print('Press "q" to Quit: ')
 
 	# Loop start
 
 	while not close:
 		now = asctime(localtime()) # Current local time 
+		#time = localtime()
+		#now = str(time()[0]) + "-" + str(time()[1]) + "-" + str(time()[2]) + " " + str(time()[3]) +
 
-		if int(now.split(" ")[3].split(":")[1]) % 2 == 0 and first:
-			request = ReadHoldingRegistersRequest(address, count)
-			response = client.execute(request)
-			print("[" + now + "] " + str(response.registers))
+		if int(now.split(" ")[3].split(":")[1]) % delay == 0 and first or keyboard.is_pressed('a'):			#debug
+
+			if count == 0:
+				request = ReadHoldingRegistersRequest(address, count)
+				#request = ReadCoilsRequest(address, 0)														#debug
+				response = client.execute(request)
+				print("[" + now + "] " + str(response))
+
+			else:
+				for _ in range(count):
+					request = ReadHoldingRegistersRequest(address, count)
+					#request = ReadCoilsRequest(address, 0)													#debug
+					response = client.execute(request)
+					print("[" + now + "] " + str(response))
+					address += 1
+
 			first = not first
-		
-		elif int(now.split(" ")[3].split(":")[1]) % 10 == 0 and not first:
+
+		elif int(now.split(" ")[3].split(":")[1]) % delay == 0 and not first:
 			first = True
 
 		# Key pressed event check
@@ -67,6 +83,21 @@ def read_hr_request(address, count, client):
 				close = not close
 		except:
 			pass
+
+def register_index(sector, type, index):
+	"""Function that return the register index of a specific sensor
+
+	sector: The number of sector where the sensor is installed
+	type: A string that identifies the type of sensor (es. Thermometer - "T")
+	index: The index of the sensor (if there are moresensor of the same type)
+	"""
+	for line in open('./registers_suddivision.txt'):
+		elements = line.split(','):
+
+		if int(elements[0]) == sector and elements[1] == type and int(elements[2]) == index:
+			return int(elements[3])
+
+	return None
 
 	
 if __name__ == "__main__":
