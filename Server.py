b80aae3bf467a931957369e3194df19d5859be76
GiacomoTezza <tezza.giacomo01@gmail.com>
2018-06-28 14:39:24 +0200

    A little change for the Server a big change for the humanity

--- Server.py
+++ Server.py
@@ -31,31 +31,14 @@ def start_server(address="0.0.0.0", port=502):
 
 
 
-	block1 = ModbusSequentialDataBlock(0, [0]*4)
+	block1 = ModbusSequentialDataBlock(0, [0]*10000)
 	floor1 = ModbusSlaveContext(hr=block1)
-	block2 = ModbusSequentialDataBlock(4, [0]*6)
-	floor2 = ModbusSlaveContext(hr=block2)
 
 	devices = {
-	0X01 : floor1,
-	0X02 : floor2
+	0X01 : floor1		
 	}
 
 	context = ModbusServerContext(devices, single=False)
-
-
-
-	# Random values
-
-
-	random_value(devices[0X01], 3, 0, 4)
-	if devices[0X01].validate(3, 0, 4):
-		print(devices[0X01].getValues(3, 0, 4))
-
-	random_value(devices[0X02], 3, 4, 6)
-	if devices[0X02].validate(3, 4, 6):
-		print(devices[0X02].getValues(3, 4, 6))
-
 
 	# Identity
 
@@ -69,22 +52,6 @@ def start_server(address="0.0.0.0", port=502):
 
 	server = StartTcpServer(context, identity, (address, port))
 
-
-def random_value(device, function, address=0, count=0):
-	"""A function that randomize the value of the given registers
-	device: The istance of the slave to use
-	function: The function to use es. 0 for the coil 
-	address: The starting address of the registers
-	count: The number of register to randomize after the first one
-	"""
-	if count == 0:
-		device.setValues(function, address, [randint(10, 30)])
-
-	else:
-		for _ in range(count):
-			device.setValues(function, address, [randint(10, 30)])
-			address += 1
-
-
 if __name__ == "__main__":
 	start_server()
+		
