30eda1a1903f8f6d3dd88c3566f04292e2da40a4
GiacomoTezza <tezza.giacomo01@gmail.com>
2018-06-28 16:59:32 +0200

    MIEIR-_

--- ReadingClient.py
+++ ReadingClient.py
@@ -32,22 +32,77 @@ def start_client(address, port=502):
 	client = ModbusTcpClient(address, port)
 	client.connect()
 
+	key_help()
+	
+	close = False
+	while not close:
+		try:
+			if keyboard.is_pressed('q'):
+				close = not close
+			elif keyboard.is_pressed('s'):		# Sector
+				try:
+					sector_in = int(input("Insert the sector index: "))
+					sector_ = read_register_by_sector(client, sector_in)
+					key_help()
+					print('\nSector '+str(sector_in)+': ')
+					for i in range(len(sector_)):
+						print('\tSensor '+ str(i) + ': ' + str(sector_[i]))
+					print()
+				except:
+					key_help()
+					print('Input not valid, please retry: ')
+			elif keyboard.is_pressed('t'):		# Type
+				try:
+					type_in = str(input("Insert the type of sensor letter: "))
+					type_ = read_register_by_type(client, type_in)
+					key_help()
+					print('\nType '+'"'+str(type_in)+'"'+': ')
+					for i in range(len(type_)):
+						print('\tSensor '+ str(i) + ': ' + str(type_[i]))
+					print()
+				except:
+					key_help()
+					print('Input not valid, please retry: ')
+			elif keyboard.is_pressed('r'):		# Address
+				try:
+					address_in = int(input("Insert the register index: "))
+					count_in = input("Insert the number of registers you want to read (Press 'Enter' to read only one): ")
+					if count_in	== "":
+						address_ = read_register_by_address(client, address_in)
+						key_help()
+						print('\nAddress '+str(address_in)+': ')
+						print('\tSensor: ' + str(address_[0]))
+						print()
+					else:
+						address_ = read_register_by_address(client, address_in, int(count_in))
+						key_help()
+						print('\nAddress '+str(address_in)+': ')
+						for i in range(len(address_)):
+							print('\tSensor '+ str(i) + ': ' + str(address_[i]))
+						print()
+				except:
+					key_help()
+					print('Input not valid, please retry: ')
+		except:
+			pass
+
 	client.close()
 
 
-def read_register_by_address(address, client, count=1):
-	"""Function that read a holding register every 10 minutes
-	address: the starting address of the registers
+def read_register_by_address(client, address, count=1):
+	"""Function that read one or more holding register by his address
 	client: the istance of the client to use
+	address: the starting address of the registers
 	count: the number of register to read after the first one
 	"""
 	request = ReadHoldingRegistersRequest(address, count, unit=0x01)
 	response = client.execute(request)
 	
-	return response
+	return response.registers
+
 
 def read_register_by_sector(client, sector, filename="C:/Users/rtc/Documents/GitHub/RaspberryPymodbus/registers_subdivision.txt"):
-	"""Function that read the registers by a specific sector
+	"""Function that read one or more holding registers by a specific sector
 	client: The istance of the client to use
 	sector : The number of the sector where the sensor is located
 	"""
@@ -65,11 +120,14 @@ def read_register_by_sector(client, sector, filename="C:/Users/rtc/Documents/Git
 		response = client.execute(request)
 		value_list.append(response.registers)
 
-	return value_list	
+	if value_list != []:
+		return value_list
+	else:
+		return 'An error has occoured, please retry... '	
 
 
 def read_register_by_type(client, type, filename="C:/Users/rtc/Documents/GitHub/RaspberryPymodbus/registers_subdivision.txt"):
-	"""Function that read the registers by a specific sector
+	"""Function that read one or more holding registers by a specific sector
 	client: The istance of the client to use
 	type: The type of the element, for example thermometer(T)
 	"""
@@ -87,7 +145,12 @@ def read_register_by_type(client, type, filename="C:/Users/rtc/Documents/GitHub/
 		response = client.execute(request)
 		value_list.append(response.registers)
 
-	return value_list	
+	if value_list != []:
+		return value_list
+	else:
+		return 'An error has occoured, please retry... '
+
+
 def register_index(sector, type, index):
 	"""Function that return the register index of a specific sensor
 	sector: The number of sector where the sensor is installed
@@ -101,6 +164,18 @@ def register_index(sector, type, index):
 			return eval(elements[3])
 
 	return None
-	
+
+
+def key_help():
+	"""Function that print the key help
+	"""
+	print('\n-------------------------------')
+	print('| Press "q" to quit:          |')
+	print('| Press "s" to read sector:   |')
+	print('| Press "t" to read type:     |')
+	print('| Press "r" to read register: |')
+	print('-------------------------------\n')
+
+
 if __name__ == "__main__":
 	start_client('192.168.9.101', 502)
